28ec5ee3c11502f626b9f152b3aa2b20dc614e1a
watermelonhater <watermelonhater33@gmail.com>
2025-06-05 23:34:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const swiperTop = new Swiper('.top__swiper', {
+  // Optional parameters
+  effect: 'fade',
+  autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+    const swiperAbout = new Swiper(".about__slider", {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+  },
+    });
+
+  document.querySelectorAll('.accordeon__trigger').forEach((item) =>{
+    item.addEventListener('click', () => {
+      item.parentNode.classList.toggle('accordeon__item--active')
+    })
+  });
